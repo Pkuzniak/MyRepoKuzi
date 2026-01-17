bcb5b88b4231d7c330e8a90dadd8e82b65d20042
Piotr Kuzniak <piotr.kuzniak@poczta.fm>
2026-01-17 20:41:40 +0100

    Hello Word! My first commit

---
+++ MyPython/Calc/calc.py
@@ -0,0 +1,32 @@
+choice = input ("* - Multiplication, / - Division , + - Addition, - - Subtraction, ** - Exponentiation, % - Modulo:   ")
+
+a = int(input ("First number: "))
+b = int(input ("Second number: "))
+
+if (choice == '*'):
+    print (a * b)
+    
+elif (choice == '/'):
+    
+        if (b == 0):
+            print ("Error , 0 not support")
+            
+        else:
+            print (a / b)
+
+        
+elif (choice == '+'):
+    print (a + b)
+
+elif (choice == '-'):
+    print (a - b)
+
+
+elif (choice == '**'):
+    print (a ** b)
+
+elif (choice == '%'):
+    print (a % b)
+
+else:
+    print ("Bad choice !")
